6efacef99da66f751bec6fdd025a4d9c1db6699e
Steven T <tabsteveyang@gmail.com>
2021-09-12 22:37:06 +0800

    init commit

---
+++ src/js/PhotoGridBox.js
@@ -0,0 +1,209 @@
+/*
+ * PhotoGridBox class
+ */
+function PhotoGridBox (insertPoint, imgs, imgOnClick, rowHeight, rowGap, colGap) {
+  this.insertPoint = insertPoint
+  this.imgLoader = new Image()
+  this.dom = null
+  this.imgs = []
+  this.rowHeight = rowHeight ? rowHeight : this._getRowHeight()
+  this.rowGap = rowGap ? rowGap : 5
+  this.colGap = colGap ? colGap : 5
+  this.rowLength = 0
+  this.showUnCompeleteRow = false
+  this.compeleteIndex = -1  
+  this.lastRenderedWindowOffsetWidth = window.innerWidth
+ 
+  this._initDOM()
+  if (imgOnClick) {
+     this.setImgOnClick(imgOnClick)  
+  }
+  if (imgs) {
+    this.appendImgs(imgs)
+  }
+}
+PhotoGridBox.prototype._initDOM = function () {
+  try {
+    var uniqueClassName = 'photo-grid-box-' + +new Date()
+    var className = 'photo-grid-box ' + uniqueClassName
+    this.insertPoint.innerHTML = '<div class="'+ className +'"></div>'
+    this.dom = document.querySelector('.' + uniqueClassName)
+    this.dom.style.width = this.dom.offsetWidth + 'px'
+  } catch (e) {
+    console.error(e)
+  }
+}
+PhotoGridBox.prototype._getRowHeight = function () {
+  if (window.innerWidth < 550) {
+    return 220
+  }
+  return 200
+}
+PhotoGridBox.prototype.appendImgs = function (imgs) {
+  if (Array.isArray(imgs) && imgs.length > 0) {
+    var self = this
+    imgs.map(function(img, index) {
+      self.imgs.push(img)
+    })
+    this._render()
+  } else {
+    console.error('imgs should be an array with the elements of the src of the images(String).')
+  }
+}
+PhotoGridBox.prototype._render = function () {
+  var self = this
+  if (!self.dom) self._initDOM()
+  self._addWindowResizeEvent()
+  var imgDOMs = document.querySelectorAll('.photo-block')
+  var imgIndex = self.compeleteIndex + 1
+  var imgsLength = self.imgs.length
+  var imgLoader = self.imgLoader
+  var tempRow = []
+  var tempAccumulateWidth = 0
+  imgLoader.onload = function () {
+    var imgWidth = calcWidthByRowHeight(this.width, this.height)
+    var imgAndGapWidth = imgWidth + self.colGap
+    var element = createBlockElement(this.src, imgWidth, self.rowHeight, self.rowLength)
+    addElementToTempRow(element, imgAndGapWidth)
+    if (tempAccumulateWidth >= self.dom.offsetWidth) {
+      adjustImagesInTheRow()
+      handleCompleteRow()
+    }
+    loadImage(++imgIndex, imgsLength)
+  }
+  imgLoader.onerror = function (e) {
+    loadImage(++imgIndex, imgsLength)
+  }
+  loadImage(imgIndex, imgsLength)
+  
+  function loadImage(imgIndex, imgsLength) {
+    removeIfImgBlockExist(imgIndex)
+    if (imgIndex === imgsLength) {
+      if (tempAccumulateWidth) {
+        handleUnCompleteRow()
+      }
+      return
+    }
+    var imgSrc = self.imgs[imgIndex]
+    imgLoader.src = imgSrc
+  }
+  function removeIfImgBlockExist(imgIndex) {
+    if(imgDOMs[imgIndex]) {
+      imgDOMs[imgIndex].remove()
+    }
+  }
+  function calcWidthByRowHeight(width, height) {
+    var ratio = self.rowHeight / height
+    return Math.round(width * ratio)
+  }
+  function createBlockElement(imgSrc, imgWidth, imgHeight, rowIndex) {
+    var element = document.createElement('div')
+    var rowGap = self.rowGap * rowIndex
+    var top = self.rowHeight * rowIndex + rowGap
+    var left = tempAccumulateWidth
+    element.style.backgroundImage = 'url('+ imgSrc +')'  
+    element.className = 'photo-block'
+    element.style.width = imgWidth + 'px'
+    element.style.height = imgHeight + 'px'    
+    element.style.top = top + 'px'
+    element.style.left = left + 'px'
+    if (self.imgOnClick) {
+      element.onclick = self.imgOnClick
+      element.className += ' photo-block--clickable'
+    }
+    element.innerHTML = createBlockElementChildren()
+    return element
+  }
+  function createBlockElementChildren() {
+    return '<div class="photo-block__panel"></div>'
+  }
+  function addElementToTempRow(element, imgAndGapWidth) {
+    tempAccumulateWidth += imgAndGapWidth
+    tempRow.push(element)
+  }
+  function adjustImagesInTheRow() {
+    var pad = self.dom.offsetWidth - (tempAccumulateWidth - self.colGap)
+    var padPerBlock = pad / tempRow.length
+    for(var i = 0; i < tempRow.length; i++) {
+      var element = tempRow[i]
+      var adjustWidth = parseFloat(element.style.width.replace('px', '')) + padPerBlock
+      element.style.width = adjustWidth + 'px'
+      if (i > 0) {
+        element.style.left = parseFloat(element.style.left.replace('px', '')) + padPerBlock * i + 'px'
+      }
+    }
+  }
+  function handleCompleteRow() {
+    renderRow()
+    resetTempForNextRow()
+  }
+  function handleUnCompleteRow() {
+    if (self.showUnCompeleteRow || self.rowLength === 0) {
+      renderRow(true)  
+    }
+    resetTempForNextRow()
+  }
+  function renderRow(isRowUnComplete) {
+    var rowLength = 0
+    if (!isRowUnComplete) {
+      self.rowLength += 1
+    } else {
+      rowLength += 1
+    }
+    rowLength += self.rowLength
+    var accumulateBlockHeight = self.rowHeight * rowLength + self.rowGap * (rowLength - 1)
+    self.dom.style.height = parseFloat(accumulateBlockHeight) + 'px'
+    tempRow.map(function(element) {
+      self.dom.append(element)
+      if (!isRowUnComplete) self.compeleteIndex += 1
+    })
+  }
+  function resetTempForNextRow() {
+    tempRow = []
+    tempAccumulateWidth = 0
+  }
+}
+PhotoGridBox.prototype.setImgOnClick = function (imgOnClick) {
+  this.imgOnClick = function(e) {
+    if (imgOnClick) imgOnClick(e)
+  }
+}
+PhotoGridBox.prototype.setShowUnCompleteRow = function (value) {
+  if (typeof value === 'boolean') this.showUnCompeleteRow = value
+}
+PhotoGridBox.prototype._resize = function (self) {
+  console.log('resize')
+  this.rowLength = 0
+  this.compeleteIndex = -1
+  self.dom.style.width = null
+  this._render()
+}
+PhotoGridBox.prototype._addWindowResizeEvent = function () {
+  if (this.isResizeEventAdded) return
+  var self = this
+  var delay = 300
+  var timeout
+  this._resizeEvent = function () {
+    clearTimeout(timeout)
+    timeout = setTimeout(function() {
+      if (self.lastRenderedWindowOffsetWidth !== window.innerWidth) {
+        self._resize(self)
+        self.lastRenderedWindowOffsetWidth = window.innerWidth
+      }      
+    }, delay)
+  }
+  window.addEventListener('resize', this._resizeEvent)
+  this.isResizeEventAdded = true
+}
+PhotoGridBox.prototype._removeWindowResizeEvent = function () {
+  window.removeEventListener('resize', this._resizeEvent)
+  this.isResizeEventAdded = false
+}
+PhotoGridBox.prototype.destroy = function () {
+  this._removeWindowResizeEvent()
+  this.rowLength = 0
+  this.compeleteIndex = -1
+  this.dom.remove()
+  this.dom = null
+  this.imgs = []
+}
